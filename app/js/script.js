7e7e82edbd8b3beea70fbd19bfbad8776e00228d
nadiiakhasyshyn <nadiia.khasyshyn.pm.2020@lpnu.ua>
2021-10-13 21:58:31 +0300

    21.58 13.10

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -20,3 +21,52 @@ window.onclick = function(event)
     }
   }
 }
+
+
+
+/*
+
+function(){
+  ('.element').hover(function(){
+    (this).children('li').stop(false,true).fadeIn(300);},
+      function() {
+        (this).children('li').stop(false, true).fadeOut(300);
+      }
+  }
+*/
+
+
+
+
+
+/*
+function myFunction2() {
+  document.getElementById("myElements").classList.toggle("show");
+}
+
+window.onclick = function(event)
+{
+  if (!event.target.matches('.Elements')) {
+
+    var elem = document.getElementsByClassName("element-content");
+    var i;
+    for (i = 0; i < elem.length; i++)
+    {
+      var openEl = elem[i];
+
+      if (openEl.classList.contains('show'))  {
+        openEl.classList.remove('show');
+      }
+    }
+  }
+}
+/*/
+
+/*
+$(document).ready(function (){
+  $('.element').click(function(event){
+    $('.invoice, .charts, .snippets, .widgets, .offers, .forms, .plugin, .email, .message, .notification'
+  ).toggleClass('active');
+  });
+});
+*/
